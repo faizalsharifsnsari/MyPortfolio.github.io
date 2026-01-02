040847c0ab65166456de3015a375f90ad528f61a
Faizal sharif Ansari <fankstar432@gmail.com>
2026-01-02 21:42:30 +0530

    Update script.js

--- files/script.js
+++ files/script.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed(".text", {
-    strings: ["App Development" , "Cybersecurity" , "Web Development"],
+    strings: ["Flutter Development", "Web Development"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
